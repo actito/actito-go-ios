95965935a68610722a8afb6bdfb0f1673a421ca2
João Ferreira <joao.ferreira@actito.com>
2025-12-04 08:32:15 +0000

    feat: add test environment label to app info in settings

--- Actito Go/UI/Views/Settings/SettingsView.swift
+++ Actito Go/UI/Views/Settings/SettingsView.swift
@@ -187,7 +187,14 @@ struct SettingsView: View {
                             .font(.headline)
                             .foregroundColor(Color(UIColor.secondaryLabel))
                             .multilineTextAlignment(.center)
-                        
+
+                        if Preferences.standard.appConfiguration?.environment == .test {
+                            Text(String(localized: "settings_app_test_environment_label"))
+                                .font(.caption)
+                                .foregroundColor(Color(UIColor.secondaryLabel))
+                                .multilineTextAlignment(.center)
+                        }
+
                         if #available(iOS 15.0, *) {
                             Text(verbatim: application.id)
                                 .font(.caption)
